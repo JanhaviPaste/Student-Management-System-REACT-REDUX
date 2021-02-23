957818572430cb6d4a419462677a8e6232dcc993
JanhaviPaste <32172461+JanhaviPaste@users.noreply.github.com>
2021-02-24 01:18:13 +0530

    commit

--- 12-student-redex-demo/src/component/students/DeleteStudent.js
+++ 12-student-redex-demo/src/component/students/DeleteStudent.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 class DeleteStudent extends Component {
   state = {};
+//component did mount method
   componentDidMount() {
     axios
       .delete(
